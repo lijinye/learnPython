a392bd9c527427d1e6a3aaf41696b2ec8f0a1ed9
lijinye <284875929@qq.com>
2018-02-27 23:22:26 +0800

    add

--- getID.py
+++ getID.py
@@ -7,6 +7,7 @@ url = 'https://www.qiushibaike.com/text/'
 res = requests.get(url)
 selector = etree.HTML(res.text)
 url_infos = selector.xpath('//div[starts-with(@class,"article block untagged mb15")]')
+# url_infos = selector.xpath('//div[contains(@class,"article block untagged mb15")]')
 for url_info in url_infos:
     print(url_info.xpath('div[1]/a[2]/h2/text()')[0].strip() if url_info.xpath('div[1]/a[2]/h2/text()') else url_info.xpath('div[1]/span[2]/h2/text()')[0])
     # if url_info.xpath('div[1]/a[2]/h2/text()'):
