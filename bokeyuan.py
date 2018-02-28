8d38e91c1c0471325c7b2b8437c1286455e85616
lijinye <284875929@qq.com>
2018-02-28 22:04:52 +0800

    add

--- bokeyuan.py
+++ bokeyuan.py
@@ -14,34 +14,37 @@ all_info_list = []
 
 
 def get_info(url):
-    res = requests.get(url, headers=headers, proxies=proxies)
-    selector=etree.HTML(res.text)
-    infos=selector.xpath('//*[@id="post_list"]/div')
+    res = requests.get(url, headers=headers)
+    selector = etree.HTML(res.text)
+    infos = selector.xpath('//*[@id="post_list"]/div')
     for info in infos:
-        title=info.xpath('div[2]/h3/a/text()')[0].strip()
-        print(info.xpath('div[2]/div/a'))
-        # author=re.search('<div class="post_item_foot">.*?<a .*?>(.*?)</a>',etree.tostring(info).decode('utf-8'))
-        author=info.xpath('div[2]/div/a')#[0].strip()
-        time=info.xpath('div[2]/div/text()')[0].strip()
-        comment=info.xpath('div[2]/div/span[1]/a/text()')[0].strip()
-        view=info.xpath('div[2]/div/span[2]/a/text()')[0].strip()
-        info_list=[title,author,time,comment,view]
-    all_info_list.append(info_list)
+        try:
+            title = info.xpath('div[2]/h3/a/text()')[0].strip()
+            author = info.xpath('div[2]/div/a/text()')[0].strip()
+            time = info.xpath('div[2]/div/text()')[0].strip()
+            comment = info.xpath('div[2]/div/span[1]/a/text()')[0].strip()
+            view = info.xpath('div[2]/div/span[2]/a/text()')[0].strip()
+            info_list = [title, author, time, comment, view]
+            all_info_list.append(info_list)
+        except IndexError:
+            pass
+
 
 if __name__ == '__main__':
-    urls = ['https://www.cnblogs.com/sitehome/p/{0}'.format(i) for i in range(11, 12)]
+    urls = ['https://www.cnblogs.com/sitehome/p/{0}'.format(i) for i in range(12, 13)]
     for u in urls:
         get_info(u)
-    header=['title','author','time','comment','view']
-    book=xlwt.Workbook(encoding='utf-8')
-    sheet=book.add_sheet('sheet1')
+    header = ['title', 'author', 'time', 'comment', 'view']
+    book = xlwt.Workbook(encoding='utf-8')
+    sheet = book.add_sheet('sheet1')
     for h in range(len(header)):
-        sheet.write(0,h,header[h])
-    i=1
+        sheet.write(0, h, header[h])
+    i = 1
+    print(all_info_list)
     for list in all_info_list:
-        j=0
+        j = 0
         for data in list:
-            sheet.write(i,j,data)
-            j+=1
-        i+=1
+            sheet.write(i, j, data)
+            j += 1
+        i += 1
     book.save('bokeyuan.xls')
